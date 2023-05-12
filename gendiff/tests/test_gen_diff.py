be58a74c5ee08f67edb53f6e809b9a2c10871448
imbalans <imba.yeah@yandex.ru>
2023-05-13 01:14:40 +0300

    step 4

---
+++ gendiff/tests/test_gen_diff.py
@@ -0,0 +1,8 @@
+from gendiff.generate_diff import generate_diff
+import json
+
+
+# def test_generate_diff():
+#     file1 = json.load(open('gendiff/files/json/file1.json'))
+#     file2 = json.load(open('gendiff/files/json/file2.json'))
+#     assert generate_diff(file1, file2) ==
